ef1f02fd3beca394013409d3fb8e5ee39b372040
bumer7721 <bumer7721@gmail.com>
2013-07-22 21:18:37 +0300

    Create test HELLO WORLD

---
+++ UD/src/main/java/com/ud/main/MainApp.java
@@ -0,0 +1,17 @@
+package com.ud.main;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainApp 
+{
+	public static Scanner cin= new Scanner(System.in);
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        cin.nextBigInteger();
+    }
+}
