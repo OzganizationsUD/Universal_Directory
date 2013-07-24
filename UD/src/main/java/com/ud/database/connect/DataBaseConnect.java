8155f3f715136a01b9af920485b201b32ebd7cd1
bumer7721 <bumer7721@gmail.com>
2013-07-24 23:23:35 +0300

    Create connoction to Data base

---
+++ UD/src/main/java/com/ud/database/connect/DataBaseConnect.java
@@ -0,0 +1,46 @@
+package com.ud.database.connect;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DataBaseConnect {
+	@Autowired
+	private DataSource ds;
+	private Connection con = null;
+	private Statement stmt = null;
+   
+	public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+
+	public Statement getStmt() {
+		return stmt;
+	}
+
+	public void setStmt(Statement stmt) {
+		this.stmt = stmt;
+	}
+
+	public void init() throws SQLException {
+		con = ds.getConnection();
+		if (con != null)
+			System.out.println("Driver load!!!");
+		else
+			System.out.println("Driver not found!!!");
+		stmt = (Statement) con.createStatement();
+
+		if (stmt != null)
+			System.out.println("Connect: ");
+		else
+			System.out.println("NO connect: ");
+	}
+}
--- UD/src/main/java/com/ud/main/MainApp.java
+++ UD/src/main/java/com/ud/main/MainApp.java
@@ -2,16 +2,19 @@ package com.ud.main;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MainApp 
 {
 	public static Scanner cin= new Scanner(System.in);
-    public static void main( String[] args )
+	
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/application-context.xml");
+
         cin.nextBigInteger();
     }
 }
